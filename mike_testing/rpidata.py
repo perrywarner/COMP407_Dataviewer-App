4d40674d50cf3ef6b290e409cb7263acdc0ea969
COMP407Group <m1black@bridgew.edu>
2018-05-07 11:02:08 -0400

    Mike add RPi sensor data

---
+++ mike_testing/rpidata.py
@@ -0,0 +1,19 @@
+import socket
+
+def rpi():
+	s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	s.bind(("159.203.78.94",12345))
+	s.listen(5)
+	c=s.accept()
+
+	datfile=open("/var/www/html/weather/rpidata.txt","a")
+
+	print("client is connected")
+
+	while True:
+		b=c[0].recv(1)
+		print(b)
+		datfile.write(b.decode())
+		datfile.flush()
+
+rpi()
