ec763b05e09a88d075795b4572d69536aaa56f55
Perry Warner <pwarner@student.bridgew.edu>
2018-05-07 10:31:37 -0400

    Add sample graph viewer

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/graph', function(req, res){
+  res.render('graph', { title: 'Graph'});
+});
+
 module.exports = router;
